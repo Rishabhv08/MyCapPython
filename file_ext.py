604649b73b485de8095c03b60b1a19f481c8f4e8
Rishabhv08 <97098520+Rishabhv08@users.noreply.github.com>
2022-01-07 09:10:45 +0530

    Add files via upload

---
+++ file_ext.py
@@ -0,0 +1,15 @@
+print("Enter File Name : ")
+filename=input()
+if ".py" in filename :
+    print("PYTHON")
+elif ".c" in filename :
+    print("C")
+elif ".html" in filename :
+    print("HTML")
+elif ".pdf" in filename :
+    print("PDF")
+elif ".php" in filename :
+    print("PHP")
+    
+
+#Alot of file extensions exist but i have used some of them for this code
